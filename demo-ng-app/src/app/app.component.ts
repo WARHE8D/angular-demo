d2fa7055caf426698452d881b2c3640b2ae8e193
WARHE8D <59991459+WARHE8D@users.noreply.github.com>
2025-03-10 15:36:53 -0600

    header component

--- demo-ng-app/src/app/app.component.ts
+++ demo-ng-app/src/app/app.component.ts
@@ -2,15 +2,22 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', //this is the tag name that will be used in index.html
   imports: [RouterOutlet],
   template: `
-    <h1>Welcome to {{title}}!</h1>
+    <h1>This is the component attached to app-root of {{ html }}!</h1>
 
     <router-outlet />
   `,
-  styles: [],
+  // this is the css that will be applied to the component only (not globally)
+  styles: [
+    `
+      h1 {
+        color: blue;
+      }
+    `,
+  ],
 })
 export class AppComponent {
-  title = 'demo-ng-app';
+  html = 'index.html';
 }
